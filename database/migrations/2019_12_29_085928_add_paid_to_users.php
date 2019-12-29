5a4b7b7b09d212d12fa6a977e3fb0ba770fcdec1
PengHueiSin <eason12326@gmail.com>
2019-12-29 17:13:32 +0800

    新增 user 資料表欄位

---
+++ database/migrations/2019_12_29_085928_add_paid_to_users.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPaidToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('gender');
+            $table->string('job_name')->nullable(true);
+            $table->string('postal_code')->nullable(true);
+            $table->string('address')->nullable(true);
+            $table->date('birthday');
+            $table->string('id_number');
+            $table->string('phone');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('gender');
+            $table->dropColumn('job_name');
+            $table->dropColumn('postal_code');
+            $table->dropColumn('address');
+            $table->dropColumn('birthday');
+            $table->dropColumn('id_number');
+            $table->dropColumn('phone');
+            $table->dropColumn('type');
+        });
+    }
+}
