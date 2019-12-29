cce1ca7a653b54c64156823c16b12e749da1b9d7
PengHueiSin <eason12326@gmail.com>
2019-12-29 16:28:48 +0800

    建立Reservation model(包含相關migration、controller)、資料表欄位規劃

---
+++ database/migrations/2019_12_29_080107_create_reservations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->dateTime('checkin');
+            $table->dateTime('checkout');
+            $table->dateTime('book_start')->nullable(true);
+            $table->dateTime('book_end')->nullable(true);
+            $table->dateTime('cancel')->nullable(true);
+            $table->unsignedInteger('discount')->nullable(true);
+            $table->unsignedInteger('total');
+            $table->text('need')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
