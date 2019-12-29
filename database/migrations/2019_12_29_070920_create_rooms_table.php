62da243ec287a2e086954a7407e3de00a26c9a55
PengHueiSin <eason12326@gmail.com>
2019-12-29 15:34:13 +0800

    建立Room model(包含相關migration、controller)、資料表欄位規劃

---
+++ database/migrations/2019_12_29_070920_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type');
+            $table->unsignedInteger('price');
+            $table->string('photo')->nullable(true);
+            $table->unsignedInteger('quantity');
+            $table->text('description')->nullable(true);
+            $table->boolean('shelves')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
