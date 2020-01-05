7753a7cbd042d70e95e6194c4aac1914f7b68516
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2020-01-05 21:05:26 +0800

    新增 ReservationRequest，驗證輸入資料是否正確

---
+++ database/factories/ReservationsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\reservations;
+use Faker\Generator as Faker;
+
+$factory->define(reservations::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
