111238aabbee6dfb7c1c2907b9647be3b251ee61
PengHueiSin <eason12326@gmail.com>
2020-01-06 23:37:07 +0800

    修改背景圖

--- resources/views/reservation/show.blade.php
+++ resources/views/reservation/show.blade.php
@@ -1,32 +1,35 @@
 @extends('layouts.master')
-@section('title', $reservations->id)
+
+@section('title', 'Blue Owl-Reservation')
+
 @section('content')
 
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('img/reservationbg.jpg')">
+    <header class="masthead" style="background-image: url('../img/reservation01.jpg')">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
                     <div class="site-heading">
                         <h1>訂單編號：00{{ $reservations->id }}</h1>
+                        <span class="subheading">Reservation</span>
                     </div>
                 </div>
             </div>
         </div>
     </header>
 
-    <h1>訂單明細</h1>
+    <h3>訂單明細</h3>
     <div class="card-header text-white @if($reservations->closed) bg-secondary @else bg-info @endif">
         訂單日期：{{ $reservations->created_at}}
         <span class="float-right">
-                        訂單狀態：
-                    @if($reservations->closed)
+            訂單狀態：
+            @if($reservations->closed)
                 已完成
             @else
                 處理中
             @endif
-                        </span>
+        </span>
     </div>
     @foreach($items as $item)
         @if($item->reservation_id == $reservations->id)
@@ -77,4 +80,5 @@
             <td>共計 {{ $reservations->total }} 元</td>
         </tr>
     </table>
+
 @endsection
