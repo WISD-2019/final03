5596bf8faa9a4bf71fa0067565ba340be50209c1
PengHueiSin <eason12326@gmail.com>
2020-01-07 19:26:01 +0800

    測試用使用者及管理者資料sql

---
+++ sql/users.sql
@@ -0,0 +1,6 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `gender`, `job_name`, `postal_code`, `address`, `birthday`, `id_number`, `phone`, `type`) VALUES
+(10,	'彭惠昕',	's3a632068@student.ncut.edu.tw',	NULL,	'$2y$10$i3OHF5Mrmy/ukRWH5DeMaOWLbRzarXx0gzUkgEZRW80N/QNtcBHf6',	NULL,	'2020-01-07 02:03:01',	'2020-01-07 02:03:01',	0,	NULL,	'970',	'花蓮縣富裕七街',	'1999-05-13',	'U221712345',	'0983123456',	'1'),
+(11,	'測試管理',	'test0000@gmail.com',	NULL,	'$2y$10$R6pc2habq6l5bRyBbkGm8u46gXh2LEdxV3WtJwUiu3Cg8z1jnjmu.',	NULL,	'2020-01-07 18:21:50',	'2020-01-07 18:21:50',	0,	NULL,	'970',	'台中市太平區中山路',	'2015-07-16',	'A12345678',	'0983123455',	'0'),
+(12,	'姚佳心',	's3a632055@student.ncut.edu.tw',	NULL,	'$2y$10$CaBZKuHZf8g0EQfDIzNjE.K4Y4MQBKK/uOZkZS8l6ld1g9immTj7e',	NULL,	'2020-01-07 19:22:40',	'2020-01-07 19:22:40',	0,	NULL,	'888',	'台中市太平區',	'1999-04-10',	'A221234567',	'0983123455',	'1');
